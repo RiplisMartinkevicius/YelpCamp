9e21be7c47d6937cf8d371d93c4d246be10ee1b7
Riplis Martinkevicius <riplis.martinkevicius@gmail.com>
2023-06-16 18:06:58 +0300

    first commit

---
+++ seeds/index.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
+const Campground = require("../models/campground");
+
+mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Datbase connected");
+});
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 300; i++) {
+    const random100 = Math.floor(Math.random() * 100);
+    const price = Math.floor(Math.random() * 20) + 10;
+    const camp = new Campground({
+      author: "647f1326f74c1a0fafba62dd",
+      location: `${cities[random100].city}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      description:
+        "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad fugiat vero sequi pariatur odit dolorem, reiciendis recusandae voluptatibus sapiente quibusdam quisquam, quae laborum iure temporibus aperiam! Doloremque neque tempore illo.",
+      price,
+      geometry: {
+        type: "Point",
+        coordinates: [cities[random100].lng, cities[random100].lat],
+      },
+      images: [
+        {
+          url: "https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png",
+          filename: "YelpCamp/ahfnenvca4tha00h2ubt",
+        },
+        {
+          url: "https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ruyoaxgf72nzpi4y6cdi.png",
+          filename: "YelpCamp/ruyoaxgf72nzpi4y6cdi",
+        },
+      ],
+    });
+    await camp.save();
+  }
+};
+
+seedDB().then(() => {
+  mongoose.connection.close();
+});
