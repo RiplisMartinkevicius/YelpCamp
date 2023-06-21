c7b1a8348983e0758f0f5515013aaf646f5effc6
Riplis Martinkevicius <riplis.martinkevicius@gmail.com>
2023-06-21 15:19:17 +0300

    Add new images

--- seeds/index.js
+++ seeds/index.js
@@ -22,32 +22,55 @@ db.once("open", () => {
 
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
+const img = [
+  {
+    url: "https://res.cloudinary.com/dgjvbnsdm/image/upload/v1687339509/YelpCamp/pcnvjbz6c1tac3osw0yq.jpg",
+    filename: "YelpCamp/pcnvjbz6c1tac3osw0yq",
+  },
+  {
+    url: "https://res.cloudinary.com/dgjvbnsdm/image/upload/v1687342588/YelpCamp/dnmjdpbdopbjyzvi5bdf.jpg",
+    filename: "YelpCamp/dnmjdpbdopbjyzvi5bdf",
+  },
+  {
+    url: "https://res.cloudinary.com/dgjvbnsdm/image/upload/v1687339968/YelpCamp/mdxskxfhqmdmul9xjiny.jpg",
+    filename: "YelpCamp/mdxskxfhqmdmul9xjiny",
+  },
+  {
+    url: "https://res.cloudinary.com/dgjvbnsdm/image/upload/v1687340009/YelpCamp/avvihk47iwbzuf4ttj1y.jpg",
+    filename: "YelpCamp/avvihk47iwbzuf4ttj1y",
+  },
+  {
+    url: "https://res.cloudinary.com/dgjvbnsdm/image/upload/v1687340143/YelpCamp/ity80yfwgfcx999cf38v.jpg",
+    filename: "YelpCamp/ity80yfwgfcx999cf38v",
+  },
+  {
+    url: "https://res.cloudinary.com/dgjvbnsdm/image/upload/v1687342661/YelpCamp/hrlbhbvgpiwcem9tzhg0.jpg",
+    filename: "YelpCamp/hrlbhbvgpiwcem9tzhg0",
+  },
+  {
+    url: "https://res.cloudinary.com/dgjvbnsdm/image/upload/v1687340304/YelpCamp/u5eljlmyjrtbayndxznr.jpg",
+    filename: "YelpCamp/u5eljlmyjrtbayndxznr",
+  },
+];
+
 const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 300; i++) {
     const random100 = Math.floor(Math.random() * 100);
+    const randomImage = img[Math.floor(Math.random() * img.length)];
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
-      author: "647f1326f74c1a0fafba62dd",
+      author: "6492ae5d17b604a36f141fc6",
       location: `${cities[random100].city}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       description:
-        "Immerse yourself in Lithuania's natural beauty at our enchanting campsite. Experience traditional charm, warm hospitality, and breathtaking landscapes. Discover the magic of Lithuanian culture while exploring pristine forests and indulging in authentic cuisine. Book your stay and let Lithuania's spirit captivate you.",
+        "Immerse yourself in Lithuania's enchanting campsite, surrounded by natural beauty. Experience warm hospitality, breathtaking landscapes, and the magic of Lithuanian culture. Explore pristine forests, indulge in authentic cuisine, and let Lithuania's spirit captivate you.",
       price,
       geometry: {
         type: "Point",
         coordinates: [cities[random100].lng, cities[random100].lat],
       },
-      images: [
-        {
-          url: "https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png",
-          filename: "YelpCamp/ahfnenvca4tha00h2ubt",
-        },
-        {
-          url: "https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ruyoaxgf72nzpi4y6cdi.png",
-          filename: "YelpCamp/ruyoaxgf72nzpi4y6cdi",
-        },
-      ],
+      images: [randomImage],
     });
     await camp.save();
   }
